a0bec71eca495938d64e840979136fa1e80b44b8
sergioimperador <allbuybr@gmail.com>
2019-02-21 17:18:33 -0300

    carrinho de compra

--- djangoecommerce/urls.py
+++ djangoecommerce/urls.py
@@ -13,21 +13,26 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.urls import path, include										 
 from django.contrib import admin
-from django.urls import path, include
-from core.views import index, contact
-from catalog import views as views_catalog
-from accounts import views as views_accounts
+from django.conf import settings
+from django.views.static import serve as serve_static
+
+#from catalog import views as views_catalog
+#from accounts import views as views_accounts
+#from checkout import views as views_checkout
 from django.contrib.auth import views as auth_views
 
-app_name = 'core'
+from core import views					  
+
 
 urlpatterns = [
-	path('', index, name='index'),
-	path('contato/', contact, name='contact'),
+	path('', views.index, name='index'),
+	path('contato/', views.contact, name='contact'),
 	path('entrar/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('sair/', auth_views.LogoutView.as_view(template_name='index.html'), name='logout'),
     path('catalogo/', include('catalog.urls', namespace='catalog')),
     path('conta/', include('accounts.urls', namespace='accounts')),
+	path('compras/', include('checkout.urls', namespace='checkout')),																  
     path('admin/', admin.site.urls),
 ]
