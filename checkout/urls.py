5371a16b6e462f7ee95ac30c3c8add8d33a6e969
sergioimperador <allbuybr@gmail.com>
2019-02-22 15:30:48 -0300

    pedido de compra

--- checkout/urls.py
+++ checkout/urls.py
@@ -9,4 +9,5 @@ app_name = 'checkout'
 urlpatterns = [
 	path('carrinho/adicionar/<slug>/', views.create_cartitem, name='create_cartitem'),
 	path('carrinho/', views.cart_item, name='cart_item'),
+	path('finalizando/', views.checkout, name='checkout'),
 ]
